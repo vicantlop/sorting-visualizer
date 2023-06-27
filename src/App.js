d1f521b33ade23a91b64d9b6272a752fefa7210e
vicantlop <vicantlop@gmail.com>
2023-06-27 10:37:44 -0700

    created columns

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import Columns from './components/Columns';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div class="container-fluid text-center border border-success border-5">
+        <button type="button" class="btn btn-success">10</button>
+        <button type="button" class="btn btn-danger">50</button>
+        <button type="button" class="btn btn-warning">100</button>
+        <Columns />
+      </div>
     </div>
   );
 }
---
+++ src/components/Columns.js
@@ -0,0 +1,17 @@
+const Columns = () => {
+    return (
+        <div class="row flex-nowrap m-5">
+            <div class="col bg-success m-1 border border-danger border-5">
+                1
+            </div>
+            <div class="col bg-danger m-1 border border-warning border-5">
+                1
+            </div>
+            <div class="col bg-warning m-1 border border-success border-5">
+                1
+            </div>
+        </div>
+    )
+}
+
+export default Columns;
